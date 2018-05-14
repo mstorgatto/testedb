084b259acb482ab43d69f5cdf38b092afd3d7e9b
Marina Storgatto <mstorgatto@gmail.com>
2018-05-14 16:42:56 -0300

    com teste

--- ex01/src/main/java/verificationpoints/LoginVerificationPoint.java
+++ ex01/src/main/java/verificationpoints/LoginVerificationPoint.java
@@ -1,5 +1,26 @@
 package verificationpoints;
 
-public class LoginVerificationPoint {
+import org.openqa.selenium.WebDriver;
 
+public class LoginVerificationPoint {
+	private WebDriver driver;
+	
+	 public LoginVerificationPoint (WebDriver driver) {
+	 this.driver = driver;
+	 }
+	 
+	 public void checkHelloMessage() {
+		 final String expectedMessage = "Hi demo";
+		 
+		 if(this.driver.getPageSource().contains(expectedMessage)) {
+			 
+			 System.out.println("Entrou");
+		 }
+		 
+	 }
+	 
+	
+	
 }
+
+//https://github.com/thvieira/treinamento-selenium
