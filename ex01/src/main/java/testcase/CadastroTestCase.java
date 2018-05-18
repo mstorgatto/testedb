ae584202d377dddfc3f589f2f6bf36c43dc35106
Marina Storgatto <mstorgatto@gmail.com>
2018-05-18 16:17:17 -0300

    ultimo

--- ex01/src/main/java/testcase/CadastroTestCase.java
+++ ex01/src/main/java/testcase/CadastroTestCase.java
@@ -4,8 +4,10 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 import Task.CadastroTask;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class CadastroTestCase {
 
@@ -14,6 +16,8 @@ public class CadastroTestCase {
 	
 	@Before
 	public void setUp() {
+		WebDriverManager.chromedriver().setup();
+		this.driver = new ChromeDriver();
 		this.driver.get("http://www.xiru.rs/d/trocas/homolog/usuarios/cadastrar");
 		this.driver.manage().window().maximize();
 		this.cadastro = new CadastroTask(driver);
@@ -22,15 +26,14 @@ public class CadastroTestCase {
 	@Test
 	public void testMain() {
 		
-		cadastro.PreencherCadastro("Maria", "teste@teste", "minhasenha", "minhasenha", "999999999", "Porto Alegre");
-			
+		this.cadastro.PreencherCadastro("Maria", "teste@teste", "minhasenha", "minhasenha", "999999999", "Porto Alegre");
+		this.cadastro.EnviarFormulario();	
 		
 	}
 		
-	@After
-	public void tearDown() {
-		
-	}
+	//@After
+	//public void tearDown() {
+	//}
 	
 	
 }
